c58d8aab2ca5394284c5c4f46b1ad7a6e37869c2
Idiakose O. Sunday <idiakosesunday@gmail.com>
2019-12-01 21:43:34 +0100

    Pull in bootsrap

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,7 @@
          <meta name="csrf-token" content="{{ csrf_token() }}">
          <title>Tasksman</title>
          <!-- Styles -->
+         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
          <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
